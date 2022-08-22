d5e13ebdfaed39086e23ee1495e5ef7936982892
pseudoname <90123698+pseudoname23@users.noreply.github.com>
2022-08-22 16:12:07 -0500

    prove that everything is broken

--- scripts/aftervars.js
+++ scripts/aftervars.js
@@ -1,4 +1,7 @@
 onresize();
 addEventListener('tick', updatePlayer);
-const player = new Player(1, 1, 1, 1);
+const player = new Player(0, 0, 1, 1);
+new SolidRect(-3, -1, 6, 2);
+
+drawAll();
 tick();
