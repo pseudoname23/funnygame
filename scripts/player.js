521bfe82d963233dfb8076a9080f62206182f1c3
pseudoname23 <ryan.c.deken@gmail.com>
2022-08-22 19:47:10 -0500

    who needs documentation anyways

--- scripts/player.js
+++ scripts/player.js
@@ -5,22 +5,16 @@ class Player {
     this.grounded = false;
   }
 
-  // Draws and labels the player's internal rectangle.
-  // returns undefined
   draw() {
     this.rect.draw(ctxs.mobile);
     this.rect.labelCoordinates(ctxs.mobile);
     this.rect.labelCenter(ctxs.mobile);
   }
 
-  // Erases the player's internal rectangle.
-  // returns undefined
   erase() {
     this.rect.erase(ctxs.mobile);
   }
 
-  // Updates the player's position and velocity.
-  // returns undefined
   updatePosition() {
     this.rect.translate(this.vx, this.vy);
     if (this.grounded) {
@@ -31,11 +25,8 @@ class Player {
   }
 }
 
-// Updates the player's position and redraws the player accordingly.
-// returns undefined
 function updatePlayer() {
   player.erase()
   player.updatePosition();
   player.draw();
-  //console.log(`${player.rect.y} tiles = ${tileCoordToPixelCoord(player.rect.y, true)} px`)
 }
