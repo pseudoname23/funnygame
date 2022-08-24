4d077e9529b270fd0df277cf1419c532c3c7a947
pseudoname <90123698+pseudoname23@users.noreply.github.com>
2022-08-23 22:45:33 -0500

    Update aftervars.js

--- scripts/aftervars.js
+++ scripts/aftervars.js
@@ -1,5 +1,6 @@
 addEventListener('tick', updatePlayer);
 const player = new Player(0, 0, 1, 1);
-new SolidRect(-3, -4, 7, 2);
+new SolidRect(-3, -4, 7, 3);
+new SolidRect(4, -4, 2, 5.5);
 onresize();
 tick();
