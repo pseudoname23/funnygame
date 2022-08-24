76b3244882bd24e3ff35678bbe464db56e8c98f8
pseudoname <90123698+pseudoname23@users.noreply.github.com>
2022-08-23 22:47:29 -0500

    m o v e

--- scripts/movement.js
+++ scripts/movement.js
@@ -1,5 +1,28 @@
-new BindableFunction('jump', true, function(){
-  if (!player.canJump) return;
-  player.canJump = false;
-  player.vy = jumpHeightToVelocity(player.stats.jumpHeight);
-}).bind('Space');
+new BindableFunction('jump', true, 
+  function(){
+    if (player.airborne) return;
+    player.vy = jumpHeightToVelocity(player.stats.jumpHeight);
+    player.airborneFromJump = true;
+  }, function(){
+    if (player.vy > 0 && player.airborneFromJump) {
+      player.vy /= 2;
+      player.airborneFromJump = false;
+    }
+  }
+).bind('Space');
+
+new BindableFunction('moveRight', false, 
+  function() {
+    player.movementState.holdRight = true;
+  }, function() {
+    player.movementState.holdRight = false;
+  }
+).bind('KeyD');
+
+new BindableFunction('moveLeft', false,
+  function() {
+    player.movementState.holdLeft = true;
+  }, function() {
+    player.movementState.holdLeft = false;
+  }
+).bind('KeyA');
