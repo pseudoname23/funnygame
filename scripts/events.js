dd7dd401155be2fc291e64592a97796535b880df
pseudoname23 <ryan.c.deken@gmail.com>
2022-08-25 11:42:31 -0500

    better event handling

--- scripts/events.js
+++ scripts/events.js
@@ -27,11 +27,7 @@ class ActiveKey {
     this.ctrl = ctrl;
     this.alt = alt;
     this.shift = shift;
-    if (activeKeys[code]) {
-      console.warn('Same key pressed twice without being released');
-    } else {
-      activeKeys[code] = this;
-    }
+    if (!activeKeys[code]) activeKeys[code] = this;
   }
 }
 class BindableFunction {
@@ -58,12 +54,13 @@ class BindableFunction {
 }
 function onkeydown(ev) {
   if (!bindings[ev.code] || ev.repeat) return;
+  if (!bindings[ev.code].allowRepeat) bindings[ev.code].down();
   new ActiveKey(ev.code, ev.ctrlKey, ev.altKey, ev.shiftKey);
 }
 function onkeyup(ev) {
   if (!bindings[ev.code]) return;
   bindings[ev.code].up();
-  activeKeys[ev.code] = undefined;
+  delete activeKeys[ev.code];
 }
 function onmousedown(ev) {
   const code = mouseEventToEventCode(ev);
@@ -74,7 +71,7 @@ function onmouseup(ev) {
   const code = mouseEventToEventCode(ev);
   if (!bindings[code]) return;
   bindings[code].up();
-  activeKeys[code] = undefined;
+  delete activeKeys[code];
 }
 addEventListener('keydown', onkeydown);
 addEventListener('keyup', onkeyup);
