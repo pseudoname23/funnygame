61f2f916b28fd1638227deb2386b0ef59326bf69
pseudoname23 <ryan.c.deken@gmail.com>
2022-09-07 10:05:15 -0500

    ustdfsdgjh

--- scripts/aftervars.js
+++ scripts/aftervars.js
@@ -1,5 +1,4 @@
-const player = new Player(-0.5, -0.5, 1, 1);
-addEventListener('tick', updatePlayer);
+let player;
 new SolidRect(-4.5, -8,   9, 2  );
 new SolidRect(-6,   -4,   4, 0.5)
 new SolidRect( 2,   -1.5, 4, 0.5)
@@ -7,5 +6,12 @@ new SolidRect(-6,    1,   4, 0.5)
 new SolidRect( 2,    3.5, 4, 0.5)
 new SolidRect(-15, -10, 31, 1.5)
 onresize();
-schmooves.walljump.equip();
-tick();
+
+function start() {
+  player = new Player(-0.5, -0.5, 1, 1);
+  window.addEventListener('tick', updatePlayer);
+  schmooves.walljump.equip();
+  tick();
+}
+
+$('play').addEventListener('pointerup', start)
