2674c37f056f1e45612e882d7e9e58b6ed2b1535
pseudoname23 <ryan.c.deken@gmail.com>
2022-09-07 11:38:19 -0500

    username handling

--- scripts/aftervars.js
+++ scripts/aftervars.js
@@ -1,4 +1,3 @@
-let player;
 new SolidRect(-4.5, -8,   9, 2  );
 new SolidRect(-6,   -4,   4, 0.5)
 new SolidRect( 2,   -1.5, 4, 0.5)
@@ -6,9 +5,9 @@ new SolidRect(-6,    1,   4, 0.5)
 new SolidRect( 2,    3.5, 4, 0.5)
 new SolidRect(-15, -10, 31, 1.5)
 onresize();
+const player = new Player(-0.5, -0.5, 1, 1);
 
 function start() {
-  player = new Player(-0.5, -0.5, 1, 1);
   window.addEventListener('tick', updatePlayer);
   schmooves.walljump.equip();
   tick();
@@ -16,3 +15,10 @@ function start() {
 }
 
 $('play').addEventListener('pointerup', start);
+
+function setPlayerName() {
+  // `this` refers to the element `input#in-game-name`
+  player.name = this.value;
+}
+
+$('in-game-name').addEventListener('input', setPlayerName);
