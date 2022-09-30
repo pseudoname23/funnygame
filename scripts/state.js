0ed0da021ed659992e0d1cbd95b2440cc5d15bab
pseudoname23 <burner7567843355@gmail.com>
2022-09-30 09:35:39 -0500

    yysdvf

--- scripts/state.js
+++ scripts/state.js
@@ -1,15 +1,19 @@
 const states = {
     MAIN_MENU: 0,
     IN_GAME: 1,
-    PAUSED: 2
+    PAUSED: 2,
+    SETTINGS: 3,
+    ACCOUNT: 4
 }
 const statesIterable = [
     'MAIN_MENU',
     'IN_GAME',
-    'PAUSED'
+    'PAUSED',
+    'SETTINGS',
+    'ACCOUNT'
 ]
 function stateNumberToName(num) {
     return statesIterable[num];
 }
 
-let state = states.MAIN_MENU;
+let state = states.MAIN_MENU, isInGame = false;
